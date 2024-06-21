eaf19d2185b4d7f491498ebeb7cd1af2168c076b
AREEBASALEEM123 <166440229+AREEBASALEEM123@users.noreply.github.com>
2024-06-21 20:16:48 +0500

    MY OOP BANK PROJECT

---
+++ main.ts
@@ -0,0 +1,139 @@
+#! usr/bin/env node
+
+import inquirer from "inquirer"
+import chalk from "chalk"
+import { faker } from "@faker-js/faker"
+
+console.log("=".repeat(80))
+console.log(chalk.bold.blueBright("\n\t Welcome to --codewithAreeba-- OOP-MY-BANK\t"))
+console.log("=".repeat(80))
+
+class Customer{
+    firstName:string
+    lastName:string
+    age:number
+    gender:string
+    mobNumber:number
+    accNumber:number
+    constructor(fName:string,lName:string,age:number,gender:string,mob:number,acc:number){
+        this.firstName = fName
+        this.lastName = lName
+        this.age = age
+        this.gender = gender
+        this.mobNumber = mob
+        this.accNumber = acc
+    }
+}
+interface BankAccount{
+    accNumber:number,
+    balance:number,
+}
+class Bank{
+    customer:Customer[]=[]
+    account:BankAccount[]=[]
+
+    addCustomer(obj:Customer){
+       this.customer.push(obj)
+    }
+
+    addAccountNumber(obj:BankAccount){
+        this.account.push(obj);
+    }
+    transaction(accObj:BankAccount){
+        let newAccounts=this.account.filter((acc)=>acc.accNumber !== accObj.accNumber)
+        this.account=[...newAccounts,accObj]
+    }
+}
+let hblBank = new Bank()
+
+for(let i:number =1; i <= 3; i++){
+    let fName = faker.person.firstName("female")
+    let lName = faker.person.lastName("male")
+    let number = parseInt(faker.helpers.fromRegExp("3##########"))
+    let customer = new Customer(fName,lName,20*i,"female",number,1000 + i)
+    hblBank.addCustomer(customer)
+    hblBank.addAccountNumber({accNumber : customer.accNumber,balance:100*i})
+}
+
+async function bankService(bank:Bank){
+    let service = await inquirer.prompt([
+       { type:"list",
+        name:"select",
+        message:"Please select the service:",
+        choices:["View Balance","Cash WithDraw","Cash Deposit","Exit"]
+       }])
+
+       if(service.select=="View Balance"){
+        let response=await inquirer.prompt([
+         {
+            type:"input",
+            name:"number",
+            message:"Please Enter Your Account Number:"
+         }])
+     let account = hblBank.account.find((acc) => acc.accNumber==response.number);
+     if(!account){
+        console.log(chalk.red.bold("Invalid Account Number"))
+    }
+     if(account){
+         let name= hblBank.customer.find((item)=>item.accNumber==account?.accNumber)
+        console.log(`Dear ${chalk.blueBright(name?.firstName)} ${chalk.blueBright(name?.lastName)} your account balance is ${chalk.blueBright("$",account.balance)}`)
+    }
+}
+
+       if(service.select == "Cash WithDraw"){
+    let response=await inquirer.prompt([
+        {
+            type:"accountNum",
+            name:"number",
+            message:"Please Enter Your Account Number:",
+        }])
+        let account=hblBank.account.find((acc)=>acc.accNumber== response.number)
+        if(!account){
+            console.log(chalk.red.bold("Invalid Account Number"))
+        }
+        if(account){
+            let answer=await inquirer.prompt([
+                {
+                    type:"number",
+                    name:"rupees",
+                    message:"Please Enter Your Amount To WithDraw: $"
+                }
+            ])
+            if(answer.rupees>account.balance){
+                console.log(`${chalk.redBright("Sorry!Insufficient Balance.")}`)}
+            
+            else{let newBalance=account.balance-answer.rupees
+            bank.transaction({accNumber:account.accNumber,balance:newBalance})
+            console.log(`$${chalk.blueBright(answer.rupees)} withdraw sucessfully! Your New Balance is $${chalk.blueBright(newBalance)}`)};
+        }
+        
+        }
+
+       if(service.select == "Cash Deposit"){
+    let response=await inquirer.prompt([
+        {
+            type:"input",
+            name:"number",
+            message:"Please Enter Your Account Number:",
+        }])
+        let account=hblBank.account.find((acc)=>acc.accNumber== response.number)
+        if(!account){
+            console.log(chalk.red.bold("Invalid Account Number"))
+        }
+        if(account){
+            let answer=await inquirer.prompt([
+                {
+                    type:"number",
+                    name:"rupees",
+                    message:"Please Enter Your Amount To Deposit:"
+                }
+            ])
+            let newBalance=account.balance+answer.rupees
+            console.log(`$${chalk.blueBright(answer.rupees)} deposited in your account! Your new balance is $${chalk.blueBright(newBalance)}`)
+        }  
+    }
+       if(service.select=="Exit"){
+        console.log("Exiting")
+       }
+}        
+bankService(hblBank);
